fd4ba0120851b818b049e1a3f3e94464884acba7
drain113 <gmonmar2408@g.educaand.es>
2022-12-19 12:07:42 +0000

    test

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -23,70 +23,90 @@
 
 #----Variables-------
 
-db_user=user
-db_password=user
+DB_USER=db_user
+DB_PASSWORD=db_pass
+DB_NAME=db_name
 #---------------------
 
 #(Instalamos wget)
 dnf install wget -y
 #------------------------------------------------------------------------------
 
+# Eliminar instalaciones previas
+rm -rf /var/www/html/phpmyadmin
+rm -rf /tmp/phpMyAdmin-5.2.0-all-languages.tar.gz
+
 # Instalamos phpMyAdmin
 wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz -P /tmp
 
 # Descomprimimos el archivo, eliminamos el .gz y movemos los archivos a /usr/share/phpmyadmin
-tar xvf /tmp/phpMyAdmin-latest-all-languages.tar.gz --directory /tmp/
-mv /tmp/phpMyAdmin-5.2.0-all-languages/ /usr/share/phpmyadmin
+tar -xvf /tmp/phpMyAdmin-latest-all-languages.tar.gz --directory /tmp/
+mv /tmp/phpMyAdmin-5.2.0-all-languages/ /var/www/html/
+mv /var/www/html/phpMyAdmin-5.2.0-all-languages/ /var/www/html/phpmyadmin
+# Permisos al usuario y grupo apache
+chown -R apache:apache /var/www/html/phpmyadmin/
+chmod 777 /var/www/html/phpmyadmin
 rm /tmp/phpMyAdmin-latest-all-languages.tar.gz
 
- # Creación de un directorio temporal
-mkdir -p /var/lib/phpmyadmin/tmp
-
-# Asignación de permisos
-chmod 777 /var/lib/phpmyadmin/tmp
-
-# Permisos al usuario y grupo apache
-chown -R apache:apache /var/lib/phpmyadmin
-
 # Creamos archivo de configuración
-cp /conf/phpmyadmin.conf  /usr/share/phpmyadmin/config.inc.php
+cp /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/phpmyadmin/config.inc.php
 
 # Instalar Politicas de Python
-yum install -y policycoreutils-python-utils
+dnf install -y php-mbstring php-pecl-zip php-xml php-json php-fpm
 
-# Añadimos phpmyadmin al apache
-semanage fcontext -a -t httpd_sys_rw_content_t '/usr/share/phpmyadmin/'
-semanage fcontext -a -t httpd_sys_rw_content_t '/var/lib/phpmyadmin/'
-restorecon -Rv '/usr/share/phpmyadmin'
 
-# # Seleccionamos la contraseña 
- mysqladmin -u root password $password
+# Importamos SQL
+mysql -u root -proot < /var/www/html/phpmyadmin/sql/create_tables.sql
 
-#Importamos el script SQL
-mysql -u root < /usr/share/phpmyadmin/sql/create_tables.sql
+# Creamos usuario y DB
+
+mysql -u root -proot <<< "DROP DATABASE IF EXISTS $DB_NAME;"
+mysql -u root -proot <<< "CREATE DATABASE $DB_NAME CHARACTER SET utf8mb4;"
+
+mysql -u root -proot <<< "CREATE USER IF NOT EXISTS $DB_USER@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u root -proot <<< "GRANT ALL PRIVILEGES ON *.* TO $DB_USER@'%';"
+mysql -u root -proot <<< "FLUSH PRIVILEGES;"
 
 #------------------------------------------------------------------------------
-# Configuramos Apache
-
-# Creamos un archivo de configuración apache
-cp conf/phpmyadmin.conf /etc/httpd/conf.d/
-------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
 # Instalación Adminer
 #-----------------------------------------------------------------------------
+ # Eliminamos instalaciones previas 
+rm -rf /var/www/html/adminer
+rm -rf /tmp/adminer-4.8.1-mysql.php
 
 # Creamos un directorio para adminer
-mkdir /var/www/html/adminer
-cd /var/www/html/adminer
-
+mkdir -p /var/www/html/adminer
+chmod 777 /var/www/html/adminer
 # Descargamos la versión 4.8.1 de Adminer
-wget -O index.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php -y
+wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php -P /tmp/
 
-# Damos los permisos y asignamos grupo y usuario
-chown -R apache:apache index.php /var/www/html/adminer/
-chmod -R 775 /var/www/html/adminer/
-#------------------------------------------------------------------------------
+#Renombramos el archivo a index.php
+mv /tmp/adminer-4.8.1-en.php /tmp/index.php
+# Cambiamos los permisos
+chown -R apache:apache /tmp/index.php
+#movemos el arhivo a /var/www/html/adminer
+mv /tmp/index.php /var/www/html/adminer
 
-#Reiniciamos el servicio apache
+#-----------------------------------------------------------------------------
+# Clave BLowfish
+#-----------------------------------------------------------------------------
+
+# Crear directorio de configuración
+mkdir -p /usr/share/phpmyadmin/tmp
+chown -R apache:apache /usr/share/phpmyadmin/tmp
+
+# Copiar archivo configuración de phpmyadmin
+cp conf/config.inc.php /var/www/html/phpmyadmin/config.inc.php
+
+# Modificar permisos de la carpeta 
+chown -R apache:apache /var/www/html/
+
+#Reiniciamos el servidor apache
 systemctl restart httpd
+#-----------------------------------------------------------------------------
+
+# Creación de un archivo de configuración apache
+cp conf/phpmyadmin.conf /etc/httpd/conf.d/
+chown -R apache:apache /etc/httpd/conf.d/
